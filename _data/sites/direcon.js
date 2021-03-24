a72f1f81097abf0af9dab9d86a7ed7b2266d6654
Nejat Can Ünlü <celierea@gmail.com>
2021-03-24 11:17:00 +0300

    Update direcon.js

--- _data/sites/direcon.js
+++ _data/sites/direcon.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: "Direcon", // optional, falls back to object key
 	description: "Well, Direcon",
+	skip: false,
 	options: {
 		frequency: 60 * 2, // (in minutes), 23 hours
 	},

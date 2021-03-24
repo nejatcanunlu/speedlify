f6892705915913795d10df929e62c054272e5deb
Nejat Can Ünlü <celierea@gmail.com>
2021-03-24 11:15:32 +0300

    Create direcon.js

---
+++ _data/sites/direcon.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Direcon", // optional, falls back to object key
+	description: "Well, Direcon",
+	options: {
+		frequency: 60 * 2, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.direcon.com/"
+	]
+};
